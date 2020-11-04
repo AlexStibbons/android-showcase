ccf8de225b799b2577cb8fbd915c331706b0918d
AlexStibbons <pantic.aleks@gmail.com>
2020-11-04 17:47:44 +0100

    Add bottom nav navigation

--- feature/home/src/main/java/com/alexstibbons/showcase/home/presentation/HomeActivity.kt
+++ feature/home/src/main/java/com/alexstibbons/showcase/home/presentation/HomeActivity.kt
@@ -2,10 +2,13 @@ package com.alexstibbons.showcase.home.presentation
 
 import android.os.Bundle
 import com.alexstibbons.showcase.ColoredSysBarActivity
+import com.alexstibbons.showcase.MediaType
+import com.alexstibbons.showcase.exhaustive
 import com.alexstibbons.showcase.home.R
 import com.alexstibbons.showcase.home.injectFeature
 import com.alexstibbons.showcase.showToast
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home_temp.*
 import org.koin.android.viewmodel.ext.android.viewModel
 import kotlinx.android.synthetic.main.activity_home.activity_home_search as searchIcon
 
@@ -23,6 +26,17 @@ internal class HomeActivity : ColoredSysBarActivity() {
         injectFeature()
 
         activity_home_viewPager.adapter = fragmentAdapter
+
+        activity_home_bottom_nav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_films -> activity_home_viewPager.setCurrentItem(0, false)
+                R.id.menu_tv -> activity_home_viewPager.setCurrentItem(1, false)
+                R.id.menu_fave -> activity_home_viewPager.setCurrentItem(2, false)
+            }
+
+            true
+        }
+        activity_home_bottom_nav.selectedItemId = R.id.menu_films
 
         searchIcon.setOnClickListener {
             showToast("Search bottom dialogue pops up here now")
