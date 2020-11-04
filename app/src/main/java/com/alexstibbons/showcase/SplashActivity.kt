622882f19e44cc026420d3097160d797fe9c79f5
AlexStibbons <pantic.aleks@gmail.com>
2020-11-04 11:46:35 +0100

    Add splash screen image

--- app/src/main/java/com/alexstibbons/showcase/SplashActivity.kt
+++ app/src/main/java/com/alexstibbons/showcase/SplashActivity.kt
@@ -2,15 +2,23 @@ package com.alexstibbons.showcase
 
 import android.os.Bundle
 import com.alexstibbons.showcase.navigator.NavigateTo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : ColoredSysBarActivity() {
     override val systemBarColor: Int
-        get() = R.color.colorAccent
+        get() = R.color.colorPrimary
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        startActivity(NavigateTo.movieList(this))
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000L)
+            startActivity(NavigateTo.movieList(this@SplashActivity))
+            finish()
+        }
     }
 }
--- feature/details/src/main/java/com/alexstibbons/showcase/details/domain/Model.kt
+++ feature/details/src/main/java/com/alexstibbons/showcase/details/domain/Model.kt
@@ -29,7 +29,7 @@ internal sealed class MediaModel(
 internal fun Movie.toFilmDetails() = MediaModel.FilmDetails(
     title,
     overview,
-    poster_path,
+    poster_path ?: "",
     IMDB_BASE+imdb_id
 )
 
