7b1a42f7a9396eb7fa30e915ec08d2ee0fa4ed2c
AlexStibbons <pantic.aleks@gmail.com>
2020-11-05 19:43:28 +0100

    Add TEMP toggles to fave/unfave

--- feature/home/src/main/java/com/alexstibbons/showcase/home/presentation/recyclerView/ItemViewHolder.kt
+++ feature/home/src/main/java/com/alexstibbons/showcase/home/presentation/recyclerView/ItemViewHolder.kt
@@ -1,5 +1,6 @@
 package com.alexstibbons.showcase.home.presentation.recyclerView
 
+import android.util.Log
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
@@ -18,15 +19,27 @@ internal abstract class ItemViewHolder<T : MediaModel>(
 
 internal class MediaViewHolder(
     parent: ViewGroup,
-    private val onMediaClicked: (Int, Int) -> Unit
+    private val onMediaClicked: (Int, Int) -> Unit,
+    private val faveListener: ToggleFaves,
+    private val faveList: List<Int>
 ) : ItemViewHolder<MediaModel>(parent) {
 
     private val title = itemView.item_title
     private val image = itemView.item_image
     private val hook = itemView.item_promo
     private val genres = itemView.item_genres
+    private val faveBtn = itemView.btn_fave
+    private val context = itemView.context
+
 
     override fun bind(model: MediaModel) {
+
+        faveBtn.setOnCheckedChangeListener(null)
+        faveBtn.isChecked = false
+        if (faveList.contains(model.id)) {
+            faveBtn.isChecked = true
+        }
+
         title.text = model.title
         hook.text = model.promo
         val genreString: String = model.genreList?.joinToString { it.title } ?: "unknown"
@@ -41,6 +54,18 @@ internal class MediaViewHolder(
             .load(BASE_IMG_URL + model.imageUrl)
             .circleCrop()
             .into(image)
+
+        faveBtn.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                faveListener.addFave(model.id)
+                faveBtn.isChecked = true
+                context.showToast("${model.title} is a fave!")
+            } else {
+                faveListener.removeFave(model.id)
+                faveBtn.isChecked = false
+                context.showToast("${model.title} is no longer a fave")
+            }
+        }
 
         itemView.setOnClickListener {
             when (model) {
--- feature/home/src/main/java/com/alexstibbons/showcase/home/presentation/recyclerView/RecyclerAdapter.kt
+++ feature/home/src/main/java/com/alexstibbons/showcase/home/presentation/recyclerView/RecyclerAdapter.kt
@@ -4,13 +4,24 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.alexstibbons.showcase.MediaModel
 
+internal interface ToggleFaves {
+    fun addFave(id: Int): Boolean
+    fun removeFave(id:Int): Boolean
+}
+
 internal class RecyclerAdapter(
     private val onMediaClicked: (Int, Int) -> Unit
 ) : RecyclerView.Adapter<ItemViewHolder<MediaModel>>() {
 
     private val mediaList: MutableList<MediaModel> = ArrayList()
+    private val favesList = mutableListOf<Int>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder<MediaModel> = MediaViewHolder(parent, onMediaClicked)
+    private val faveListener = object: ToggleFaves {
+        override fun addFave(id: Int) = favesList.add(id)
+        override fun removeFave(id: Int) = favesList.remove(id)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder<MediaModel> = MediaViewHolder(parent, onMediaClicked, faveListener, favesList)
 
     override fun getItemCount(): Int = mediaList.size
 
